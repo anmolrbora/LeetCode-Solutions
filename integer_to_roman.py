b2a77d39721692bbf9ecfaa52f7650b437c67587
Anmol Bora <anmolrbora@gmail.com>
2019-09-26 14:23:49 -0400

    Create integer_to_roman.py

---
+++ integer_to_roman.py
@@ -0,0 +1,32 @@
+#https://leetcode.com/problems/integer-to-roman/
+
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        mapping = {
+            1: "I",
+            4: "IV",
+            5: "V",
+            9: "IX",
+            10: "X",
+            40: "XL",
+            50: "L",
+            90: "XC",
+            100: "C",
+            400: "CD",
+            500: "D",
+            900: "CM",
+            1000: "M",
+        }
+        l = list(mapping.keys())[::-1]
+        result = ""
+        for a in l:
+            while num>=a:
+                num -= a
+                result += mapping[a]
+        return result
+                
+        
